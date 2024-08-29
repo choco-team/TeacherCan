f6c17d096c60322f04f1a90382db8f35aa75757e
JSM <dndls321@gmail.com>
2024-08-29 11:04:11 +0900

    FEAT add tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
-export default function timer(){
-    return <h1>타이머</h1>
+export default function Page() {
+    return <div>
+        <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Button
+        </button>
+    </div>
 }
