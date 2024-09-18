d4492d6555cce7877e25d929356caef31144909a
김홍동 <57981252+nlom0218@users.noreply.github.com>
2024-09-18 23:38:40 +0900

    chore: 구조 정리 (#8)

--- src/containers/landing/menu-list/menu-list.tsx
+++ src/containers/landing/menu-list/menu-list.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { Button } from '@/components/ui/button';
+import { Button } from '@/components/button';
 
 interface Props {
   handleClickTimerButton: () => void;
