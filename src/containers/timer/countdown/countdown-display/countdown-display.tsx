21dfd39bd52881f1ea96a8ff30343c1511eb7ff6
jangjiyoon <jjswan2011@gmail.com>
2024-09-22 15:03:28 +0900

    feat: 1분 증감 버튼 추가

--- src/containers/timer/countdown/countdown-display/countdown-display.tsx
+++ src/containers/timer/countdown/countdown-display/countdown-display.tsx
@@ -1,6 +1,5 @@
 // 지윤쌤
 // 구현이 필요한 기능 목록
-// [ ] 타이머 숫자 위 아래 화살표 버튼이 있어서 1분씩은 바로 클릭이 가능하다.
 // [ ] 60분에서 넘어가면 시간 단위도 생긴다.
 // [ ] 최대 9시간 59분까지 가능하다.
 
@@ -19,24 +18,51 @@ export default function CountdownDisplay() {
 
   const buttonText = isPaused || !isActive ? 'Start' : 'Pause';
 
+  const handleIncreaseMinutes = () => {
+    updateMinutes(1, true); // 1분 증가
+  };
+
+  const handleDecreaseMinutes = () => {
+    updateMinutes(-1, true); // 1분 감소
+  };
+
   return (
     <div className="bg-white dark:bg-gray-800 shadow-lg rounded-lg p-8 w-full max-w-md">
       <Heading2 className="text-center">Countdown Timer</Heading2>
       <div className="flex items-center justify-center mb-6 space-x-4">
-        <Input
-          type="number"
-          id="minutes"
-          value={minutes}
-          onChange={(e) => updateMinutes(Number(e.target.value))}
-          className="w-1/3 text-right font-bold text-lg"
-        />
-        <Input
-          type="number"
-          id="seconds"
-          value={seconds}
-          onChange={(e) => updateSeconds(Number(e.target.value))}
-          className="w-1/3 text-right font-bold text-lg"
-        />
+        <div className="flex flex-col items-center">
+          <Button
+            onClick={handleIncreaseMinutes}
+            variant="primary-ghost"
+            className="py-1 px-1 text-icon"
+          >
+            ▲
+          </Button>
+          <Input
+            type="number"
+            id="minutes"
+            value={minutes}
+            onChange={(e) => updateMinutes(Number(e.target.value))}
+            className="w-full max-w-[120px] text-right font-bold text-lg"
+          />
+          <Button
+            onClick={handleDecreaseMinutes}
+            variant="primary-ghost"
+            className="py-1 px-1 text-icon" // 세로 길이 줄이기
+          >
+            ▼
+          </Button>
+        </div>
+        <span className="text-lg font-bold">:</span>
+        <div className="flex flex-col items-center">
+          <Input
+            type="number"
+            id="seconds"
+            value={seconds}
+            onChange={(e) => updateSeconds(Number(e.target.value))}
+            className="w-full max-w-[120px] text-right font-bold text-lg"
+          />
+        </div>
       </div>
       <div className="flex justify-center gap-4">
         <Button onClick={handlePause} variant="primary-outline">
