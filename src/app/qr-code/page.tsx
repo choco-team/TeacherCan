c287e19ad0a03c5812881b378881161eba7cb716
seoltang <yong_drg@naver.com>
2024-10-03 18:18:36 +0900

    feat(qr-code): QR 코드 페이지 추가

---
+++ src/app/qr-code/page.tsx
@@ -0,0 +1,11 @@
+import QrCodeContainer from '@/containers/qr-code/qr-code-container';
+
+export const metadata = {
+  title: 'QR Code',
+};
+
+function QrCode() {
+  return <QrCodeContainer />;
+}
+
+export default QrCode;
