c2751318fa173678ed06f969e2362e07c44bcac7
JSM000 <67637792+JSM000@users.noreply.github.com>
2024-10-30 23:39:25 +0900

    feat/ 결과숨기기 설정을 적용합니다.

--- src/containers/random-pick/random-pick-playground/playground-card/playground-result-card.tsx
+++ src/containers/random-pick/random-pick-playground/playground-card/playground-result-card.tsx
@@ -4,20 +4,27 @@ import { useRandomPickState } from '../../random-pick-provider/random-pick-provi
 
 export default function ResultCard({ winner }) {
   const { handleCardFlip } = useRandomPickPlaygroundAction();
-  const { pickList, pickType } = useRandomPickState();
+  const {
+    pickList,
+    pickType,
+    options: { isHideResult },
+  } = useRandomPickState();
 
-  return (
+  return isHideResult ? (
     <div
-      className={`relative w-30 h-20 cursor-pointer transform transition-transform duration-500 perspective-1000 ${winner.isflipped ? 'rotate-y-180' : ''}`}
+      className={`relative w-30 h-12 cursor-pointer transform transition-transform duration-500 perspective-1000 ${winner.isflipped ? 'rotate-y-180' : ''}`}
       onClick={() => handleCardFlip(winner.id)}
     >
       <div className="absolute inset-0 w-full h-full bg-primary text-white flex items-center justify-center backface-hidden rounded-lg ">
         <TeacherCanLogo width="50" />
       </div>
       <div className="absolute inset-0 w-full h-full bg-primary text-white flex items-center justify-center backface-hidden rounded-lg transform rotate-y-180">
-        {' '}
-        {pickList[pickType][winner.id].value}{' '}
+        {pickList[pickType][winner.id].value}
       </div>
+    </div>
+  ) : (
+    <div className="w-30 h-12 bg-primary text-white flex items-center justify-center rounded-lg">
+      {pickList[pickType][winner.id].value}
     </div>
   );
 }
