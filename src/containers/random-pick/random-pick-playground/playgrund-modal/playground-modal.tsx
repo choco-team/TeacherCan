f6667f9438d38e7b6ad343fdbafd6856d9629943
JSM000 <67637792+JSM000@users.noreply.github.com>
2024-10-31 23:05:09 +0900

    feat/ 모달창 2배 코드를 간단하게 바꿉니다.

--- src/containers/random-pick/random-pick-playground/playgrund-modal/playground-modal.tsx
+++ src/containers/random-pick/random-pick-playground/playgrund-modal/playground-modal.tsx
@@ -1,57 +1,48 @@
 import Modal from 'react-modal';
-import {
-  useRandomPickPlaygroundAction,
-  useRandomPickPlaygroundState,
-} from '../../random-pick-playground-provider.tsx/random-pick-playground-provider.hooks';
+import { useCallback } from 'react';
+import { useRandomPickPlaygroundState } from '../../random-pick-playground-provider.tsx/random-pick-playground-provider.hooks';
 
 import SetPickNumberModal from './modal-set-picknumber/modal-set-picknumber';
 import ResultModal from './modal-result/modal-result';
 import { MODAL_STATE_TYPES } from '../../random-pick-playground-provider.tsx/random-pick-playground-provider.constans';
 
-const overlay: React.CSSProperties = {
-  backgroundColor: ' rgba(0, 0, 0, 0.4)',
-  width: '100%',
-  height: '100vh',
-  zIndex: '10',
-  position: 'fixed',
-  top: '0',
-  left: '0',
-};
-const content: React.CSSProperties = {
-  width: '400px',
-  height: '200px',
-  zIndex: '150',
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  borderRadius: '10px',
-  boxShadow: '2px 2px 2px rgba(0, 0, 0, 0.25)',
-  backgroundColor: 'white',
-  justifyContent: 'center',
-  overflow: 'auto',
-};
-
 export default function PlaygroundModal() {
   const { modalState } = useRandomPickPlaygroundState();
-  const { selectModalState } = useRandomPickPlaygroundAction();
+  const onRequestClose = useCallback(() => {}, []);
 
   const customModalStyles: ReactModal.Styles = {
-    overlay,
-    content:
-      modalState === MODAL_STATE_TYPES.resultModal
-        ? { ...content, height: '400px', width: '800px' }
-        : content,
+    overlay: {
+      backgroundColor: ' rgba(0, 0, 0, 0.4)',
+      width: '100%',
+      height: '100vh',
+      zIndex: '10',
+      position: 'fixed',
+      top: '0',
+      left: '0',
+    },
+    content: {
+      width: modalState === MODAL_STATE_TYPES.resultModal ? '800px' : '400px',
+      height: modalState === MODAL_STATE_TYPES.resultModal ? '400px' : '200px',
+      zIndex: '150',
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      borderRadius: '10px',
+      boxShadow: '2px 2px 2px rgba(0, 0, 0, 0.25)',
+      backgroundColor: 'white',
+      justifyContent: 'center',
+      overflow: 'auto',
+    },
   };
-
   return (
     <Modal
       isOpen={modalState !== MODAL_STATE_TYPES.noModal}
-      onRequestClose={() => selectModalState(MODAL_STATE_TYPES.noModal)}
+      onRequestClose={onRequestClose}
       style={customModalStyles}
       ariaHideApp={false}
       contentLabel="Pop up Message"
-      shouldCloseOnOverlayClick
+      shouldCloseOnOverlayClick={false}
     >
       {modalState === MODAL_STATE_TYPES.setPickNumberModal && (
         <SetPickNumberModal />
