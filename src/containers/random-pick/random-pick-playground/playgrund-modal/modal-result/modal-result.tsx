878a9b7f8dfd585c5476489f4bab3746558756e5
JSM000 <67637792+JSM000@users.noreply.github.com>
2024-11-03 12:45:36 +0900

    feat/ 다시뽑기 버튼의 값도 유효성검사합니다.

--- src/containers/random-pick/random-pick-playground/playgrund-modal/modal-result/modal-result.tsx
+++ src/containers/random-pick/random-pick-playground/playgrund-modal/modal-result/modal-result.tsx
@@ -5,16 +5,59 @@ import {
 import { XIcon } from 'lucide-react';
 import { Button } from '@/components/button';
 import { MODAL_STATE_TYPES } from '@/containers/random-pick/random-pick-playground-provider.tsx/random-pick-playground-provider.constans';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Form, FormControl, FormField, FormMessage } from '@/components/form';
+import { Input } from '@/components/input';
 import ResultCard from '../../playground-card/playground-result-card';
 
 export default function ResultModal() {
-  const { winners, numberOfPick } = useRandomPickPlaygroundState();
+  const { winners, maxNumberOfPick } = useRandomPickPlaygroundState();
   const { selectModalState, runPick } = useRandomPickPlaygroundAction();
+
+  const formSchema = z.object({
+    number: z.coerce
+      .number()
+      .min(1, {
+        message: '최소 인원은 1명입니다.',
+      })
+      .max(maxNumberOfPick, {
+        message: `남은 인원은 ${maxNumberOfPick}명입니다.`,
+      }),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      number: 1,
+    },
+  });
+
+  const onSubmit = ({ number }: z.infer<typeof formSchema>) => {
+    runPick(number);
+  };
 
   return (
     <div>
       <div className="flex justify-between mb-4">
-        <Button onClick={() => runPick()}>다시뽑기 ({numberOfPick}명)</Button>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+            <FormField
+              control={form.control}
+              name="number"
+              render={({ field }) => (
+                <div className="flex gap-x-4">
+                  <FormControl>
+                    <Input type="number" {...field} />
+                  </FormControl>
+                  <Button type="submit">다시뽑기</Button>
+                  <FormMessage />
+                </div>
+              )}
+            />
+          </form>
+        </Form>
         <button
           type="button"
           onClick={() => selectModalState(MODAL_STATE_TYPES.noModal)}
