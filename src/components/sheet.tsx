3edd78497eaac272312590e23cd81ea826f44a6d
nlom0218 <nlom0218@naver.com>
2025-01-11 23:25:39 +0900

    feat: Sheet 사용시 좌측 콘텐츠 스타일 변경

--- src/components/sheet.tsx
+++ src/components/sheet.tsx
@@ -14,21 +14,6 @@ const SheetClose = SheetPrimitive.Close;
 
 const SheetPortal = SheetPrimitive.Portal;
 
-const SheetOverlay = React.forwardRef<
-  React.ElementRef<typeof SheetPrimitive.Overlay>,
-  React.ComponentPropsWithoutRef<typeof SheetPrimitive.Overlay>
->(({ className, ...props }, ref) => (
-  <SheetPrimitive.Overlay
-    className={cn(
-      'fixed inset-0 z-50 bg-black/60 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
-      className,
-    )}
-    {...props}
-    ref={ref}
-  />
-));
-SheetOverlay.displayName = SheetPrimitive.Overlay.displayName;
-
 const sheetVariants = cva(
   'fixed z-50 gap-4 bg-background px-6 py-8 overflow-scroll shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500',
   {
@@ -57,7 +42,6 @@ const SheetContent = React.forwardRef<
   SheetContentProps
 >(({ side = 'right', className, children, ...props }, ref) => (
   <SheetPortal>
-    <SheetOverlay />
     <SheetPrimitive.Content
       ref={ref}
       className={cn(sheetVariants({ side }), className)}
@@ -67,6 +51,9 @@ const SheetContent = React.forwardRef<
           event.preventDefault();
         })
       }
+      onInteractOutside={(event) => {
+        event.preventDefault();
+      }}
       {...props}
     >
       {children}
@@ -154,7 +141,6 @@ SheetDescription.displayName = SheetPrimitive.Description.displayName;
 export {
   Sheet,
   SheetPortal,
-  SheetOverlay,
   SheetTrigger,
   SheetClose,
   SheetContent,
