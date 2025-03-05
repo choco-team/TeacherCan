404041002cbf5bbc8de7cdf89c8dc4504bff6ca4
jangjiyoon <jjswan2011@gmail.com>
2025-03-05 20:15:12 +0900

    feat: 식단 조회 서비스 날짜 수정

--- src/containers/landing/lunch-menu/mealInformation/mealList.tsx
+++ src/containers/landing/lunch-menu/mealInformation/mealList.tsx
@@ -2,24 +2,52 @@ import React from 'react';
 import { CardContent } from '@/components/card';
 import MealItem from './mealItem';
 
-interface MealListProps {
+type MealListProps = {
   mealData: { MLSV_YMD: string; DDISH_NM: string }[];
-}
+};
+
+const formatDate = (
+  dateStr: string,
+): { formatted: string; isWeekend: boolean } => {
+  const year = parseInt(dateStr.slice(0, 4), 10);
+  const month = parseInt(dateStr.slice(4, 6), 10) - 1;
+  const day = parseInt(dateStr.slice(6, 8), 10);
+
+  const date = new Date(year, month, day);
+
+  const daysOfWeek = [
+    '일요일',
+    '월요일',
+    '화요일',
+    '수요일',
+    '목요일',
+    '금요일',
+    '토요일',
+  ];
+  const dayOfWeek = daysOfWeek[date.getDay()];
+
+  const isWeekend = dayOfWeek === '토요일' || dayOfWeek === '일요일';
+
+  return {
+    formatted: `${year}년 ${month + 1}월 ${day}일 ${dayOfWeek}`,
+    isWeekend,
+  };
+};
 
 function MealList({ mealData }: MealListProps) {
-  const formatDate = (dateStr: string): string => {
-    return `${dateStr.substring(0, 4)}년 ${dateStr.substring(4, 6)}월 ${dateStr.substring(6, 8)}일`;
-  };
+  const filteredMeals = mealData.filter(
+    (meal) => !formatDate(meal.MLSV_YMD).isWeekend,
+  );
 
   return (
     <div className="bg-white shadow-custom py-4 px-8 rounded-xl w-full overflow-auto">
-      {mealData.length > 0 && (
+      {filteredMeals.length > 0 && (
         <CardContent className="p-2">
           <div className="flex flex-col gap-2">
-            {mealData.map((meal) => (
+            {filteredMeals.map((meal) => (
               <MealItem
                 key={meal.MLSV_YMD}
-                date={formatDate(meal.MLSV_YMD)}
+                date={formatDate(meal.MLSV_YMD).formatted}
                 dishes={meal.DDISH_NM}
               />
             ))}
