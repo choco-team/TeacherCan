b0c9b63de8ee56ebc1b33767991e613063d61633
nlom0218 <nlom0218@naver.com>
2025-03-15 16:21:09 +0900

    feat: 학생목록, 방 정보 ui

--- src/containers/music-request/music-request-teacher/music-request-teacher-main/student-list/student-list.tsx
+++ src/containers/music-request/music-request-teacher/music-request-teacher-main/student-list/student-list.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import { nanoid } from 'nanoid';
 import { firebaseDB } from '@/services/firebase';
 import { onValue, ref } from 'firebase/database';
 import { useMusicRequestTeacherState } from '../../music-request-teacher-provider/music-request-teacher-provider.hooks';
@@ -23,9 +22,28 @@ export default function StudentList() {
   }, [params.roomId]);
 
   return (
-    <div className="rounded m-2 p-2">
-      {students &&
-        students.map((student) => <div key={nanoid()}>{student}</div>)}
+    <div className="py-4">
+      {students ? (
+        <div className="bg-gray-100 flex flex-col gap-[1px]">
+          {students.map((student, index) => (
+            <div
+              className="p-2 flex gap-2 bg-white text-gray-700"
+              // eslint-disable-next-line react/no-array-index-key
+              key={index}
+            >
+              {student}
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="flex flex-col gap-4 mt-12 justify-center items-center">
+          <div className="text-center text-sm text-gray-500">
+            <span>참여한 학생이 없어요.</span>
+            <br />
+            <span>방 정보에서 QR코드를 통해 학생을 초대해보세요.</span>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
