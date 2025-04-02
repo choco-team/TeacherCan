cf1be7aee1dc16380d8b1daf6bd95f6d937fadc8
seoltang <yong_drg@naver.com>
2025-04-03 03:18:46 +0900

    feat(notice): 알림장 기능 추가

---
+++ src/app/notice/page.tsx
@@ -0,0 +1,9 @@
+import NoticeContainer from '@/containers/notice/notice-container';
+
+export const metadata = {
+  title: '알림장',
+};
+
+export default function Notice() {
+  return <NoticeContainer />;
+}
