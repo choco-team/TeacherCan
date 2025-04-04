084280013431a94986a7379b3558eae4419e9319
nlom0218 <nlom0218@naver.com>
2025-04-04 23:07:46 +0900

    feat: 요청방법 간소화

--- src/containers/music-request/music-request-student/music-request-student-main/register-music/register-guide/register-guide.tsx
+++ src/containers/music-request/music-request-student/music-request-student-main/register-music/register-guide/register-guide.tsx
@@ -4,8 +4,10 @@ export default function RegisterGuide() {
   return (
     <div className="flex flex-col gap-4">
       <div>
-        <span className="text-lg font-bold text-primary-500">1.</span>
-        <div>유튜브 페이지에 접속합니다.</div>
+        <div className="flex items-center gap-1">
+          <span className="text-lg font-bold text-primary-500">1.</span>
+          <div>유튜브 페이지 접속 {'>'} 음악 검색</div>
+        </div>
         <div
           className="text-sm text-gray-700 underline"
           onClick={() => {
@@ -16,14 +18,11 @@ export default function RegisterGuide() {
         </div>
       </div>
       <div>
-        <span className="text-lg font-bold text-primary-500">2.</span>
-        <div>음악을 검색합니다.</div>
-      </div>
-      <div>
-        <span className="text-lg font-bold text-primary-500">3.</span>
-        <div>
-          검색 페이지에서 신청하고자 하는 음악의 더보기를 클릭 후{' '}
-          <span className="font-semibold">공유</span>를 클릭합니다.
+        <div className="flex items-center gap-1">
+          <span className="text-lg font-bold text-primary-500">2.</span>
+          <div>
+            더보기 아이콘 {'>'} 공유 {'>'} 복사 클릭
+          </div>
         </div>
         <div className="relative aspect-video">
           <Image
@@ -31,13 +30,6 @@ export default function RegisterGuide() {
             alt="음악신청 방법"
             fill
           />
-        </div>
-      </div>
-      <div>
-        <span className="text-lg font-bold text-primary-500">4.</span>
-        <div>
-          게시물로 공유에서 유튜브 음악 주소 옆{' '}
-          <span className="font-semibold">복사</span>버튼을 클릭합니다.
         </div>
         <div className="relative aspect-square">
           <Image
@@ -48,8 +40,10 @@ export default function RegisterGuide() {
         </div>
       </div>
       <div>
-        <span className="text-lg font-bold text-primary-500">5.</span>
-        <div>요청하기에서 오른쪽 아래의 아이콘을 클릭합니다.</div>
+        <div className="flex items-center gap-1">
+          <span className="text-lg font-bold text-primary-500">3.</span>
+          <div>요청하기 아이콘 클릭</div>
+        </div>
         <div className="relative aspect-[3/1.2]">
           <Image
             src="/image/music-request/request-3.png"
@@ -59,11 +53,9 @@ export default function RegisterGuide() {
         </div>
       </div>
       <div>
-        <span className="text-lg font-bold text-primary-500">6.</span>
-        <div>
-          주소를 정확히 붙여넣기 하면 유튜브 음악 아이디, 썸네일 사진, 제목을
-          확인할 수 있습니다. 마지막으로{' '}
-          <span className="font-semibold">신청하기</span>버튼을 클릭합니다.
+        <div className="flex items-center gap-1">
+          <span className="text-lg font-bold text-primary-500">4.</span>
+          <div>신청하기</div>
         </div>
         <div className="relative aspect-square">
           <Image
