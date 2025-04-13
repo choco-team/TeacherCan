304360fb329f193226065fea61115475deb1d985
nlom0218 <nlom0218@naver.com>
2025-04-13 12:55:13 +0900

    feat: sidebar 관련 라이브러리 설치

--- tailwind.config.js
+++ tailwind.config.js
@@ -47,6 +47,16 @@ module.exports = {
           DEFAULT: colors.white,
           foreground: colors.gray[900],
         },
+        sidebar: {
+          DEFAULT: 'hsl(var(--sidebar-background))',
+          foreground: 'hsl(var(--sidebar-foreground))',
+          primary: 'hsl(var(--sidebar-primary))',
+          'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+          accent: 'hsl(var(--sidebar-accent))',
+          'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+          border: 'hsl(var(--sidebar-border))',
+          ring: 'hsl(var(--sidebar-ring))',
+        },
       },
       borderRadius: {
         lg: `var(--radius)`,
