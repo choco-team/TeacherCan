6b5c88858b40137e61e8ce1083ac7d32b57dd984
JSM000 <67637792+JSM000@users.noreply.github.com>
2025-06-29 22:18:04 +0900

    feat: ping수신, 재연결 코드 작성

--- src/hooks/apis/music-request/use-music-sse.ts
+++ src/hooks/apis/music-request/use-music-sse.ts
@@ -1,37 +1,60 @@
 import { YoutubeVideo } from '@/apis/music-request/musicRequest';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 export function useMusicSSE(
   roomId: string,
   onUpdate: (musicList: YoutubeVideo[]) => void,
 ) {
+  const eventSourceRef = useRef<EventSource | null>(null);
+  const pingTimeout = useRef<NodeJS.Timeout | null>(null);
+
   useEffect(() => {
     if (!roomId) {
       return undefined;
     }
 
-    const eventSource = new EventSource(
-      `${process.env.NEXT_PUBLIC_SERVER_URL}/music-request/sse?roomId=${roomId}`,
-      {
-        withCredentials: true,
-      },
-    );
+    let reconnectTimeout: NodeJS.Timeout;
 
-    eventSource.onmessage = (event: MessageEvent) => {
-      try {
-        const parsed = JSON.parse(event.data);
-        onUpdate(parsed.musicList);
-      } catch (err) {
-        throw new Error('SSE 파싱 오류');
-      }
+    const connect = () => {
+      const es = new EventSource(
+        `${process.env.NEXT_PUBLIC_SERVER_URL}/music-request/sse?roomId=${roomId}`,
+        {
+          withCredentials: true,
+        },
+      );
+
+      es.addEventListener('music-list', (event: MessageEvent) => {
+        try {
+          const parsed = JSON.parse(event.data);
+          onUpdate(parsed.musicList);
+        } catch (err) {
+          throw new Error('SSE-musicList 파싱 오류');
+        }
+      });
+
+      es.addEventListener('ping', () => {
+        // ping 수신 → 연결 유지 확인됨 -> setTimeout 제거
+        if (pingTimeout.current) clearTimeout(pingTimeout.current);
+        pingTimeout.current = setTimeout(() => {
+          es.close();
+          connect(); // 재연결
+        }, 20000); // 20초 내 ping 없으면 재연결
+      });
+
+      es.onerror = () => {
+        es.close();
+        reconnectTimeout = setTimeout(connect, 3000);
+      };
+
+      eventSourceRef.current = es;
     };
 
-    eventSource.onerror = () => {
-      throw new Error('music-sse 연결 오류');
-    };
+    connect();
 
     return () => {
-      eventSource.close();
+      eventSourceRef.current?.close();
+      clearTimeout(reconnectTimeout);
+      clearTimeout(pingTimeout.current!);
     };
   }, [roomId, onUpdate]);
 }
