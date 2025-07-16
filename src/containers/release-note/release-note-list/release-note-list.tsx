b603c6312b76e9297563f6225678e2901c0d2e30
nlom0218 <nlom0218@naver.com>
2025-07-16 21:51:06 +0900

    feat: note item 분리

--- src/containers/release-note/release-note-list/release-note-list.tsx
+++ src/containers/release-note/release-note-list/release-note-list.tsx
@@ -1,7 +1,4 @@
-import { format } from 'date-fns';
-import { ko } from 'date-fns/locale';
-import Link from 'next/link';
-import Image from 'next/image';
+import ReleaseNoteItem from './release-note-item/release-note-item';
 import { getReleaseNote } from './release-note-list.utils';
 
 export default async function ReleaseNoteList() {
@@ -14,24 +11,8 @@ export default async function ReleaseNoteList() {
   return (
     <div className="p-6 mx-auto max-w-screen-sm w-full">
       <h1 className="text-4xl font-bold mb-8 text-gray-900">릴리즈 노트</h1>
-      <div className="flex flex-col gap-5">
-        {data?.map((item) => (
-          <Link key={item.id} href={`/release-note/${item.id}`}>
-            <div className="relative w-full aspect-video rounded-sm">
-              <Image
-                src={item.coverImageUrl}
-                alt={item.title}
-                fill
-                className="object-cover rounded-sm"
-              />
-            </div>
-            {item.title}
-            {format(new Date(item.date), 'yy년 MMM dd일 iiii', {
-              locale: ko,
-            })}
-            <button type="button">자세히 보기</button>
-          </Link>
-        ))}
+      <div className="flex flex-col gap-12">
+        {data?.map((item) => <ReleaseNoteItem key={item.id} {...item} />)}
       </div>
     </div>
   );
