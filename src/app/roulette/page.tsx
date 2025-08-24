04accee65cceaaf92cffd7b7b7b16cd2f8e8bc8b
nlom0218 <nlom0218@naver.com>
2025-08-25 06:44:01 +0900

    feat: 룰레 돌리기 페이지 공개

--- src/app/roulette/page.tsx
+++ src/app/roulette/page.tsx
@@ -1,7 +1,7 @@
 import { RouletteContainer } from '@/containers/roulette/roulette-container';
 
 export const metadata = {
-  title: '룰렛 뽑기',
+  title: '룰렛 돌리기',
 };
 
 export default function RoulettePage() {
