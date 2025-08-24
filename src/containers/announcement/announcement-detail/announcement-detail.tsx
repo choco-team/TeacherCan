f8c992084e0fd939fad63bb0fd1b79aa0680fad5
nlom0218 <nlom0218@naver.com>
2025-08-25 07:29:00 +0900

    feat: 모바일 패딩 수정

--- src/containers/announcement/announcement-detail/announcement-detail.tsx
+++ src/containers/announcement/announcement-detail/announcement-detail.tsx
@@ -144,7 +144,7 @@ export default async function AnnouncementDetail({ id }: Props) {
   const pageSummary = getPageSummary(data.page);
 
   return (
-    <div className="p-6 mx-auto max-w-screen-sm w-full">
+    <div className="mx-auto max-w-screen-sm w-full lg:p-6">
       {pageTitle && (
         <h1 className="text-4xl font-bold mb-2 text-text-title">{pageTitle}</h1>
       )}
