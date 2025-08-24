4915ceea300ebeaf7fbb40f77a9d5609305286fa
김홍동 <57981252+nlom0218@users.noreply.github.com>
2025-08-24 15:16:56 +0900

    feat: 공지사항 오픈 (#102)

--- src/app/announcement/[id]/page.tsx
+++ src/app/announcement/[id]/page.tsx
@@ -2,7 +2,11 @@ import AnnouncementContainer from '@/containers/announcement/announcement-contai
 
 export const metadata = {
   title: '공지사항',
+  description: '티처캔의 새로운 소식을 전해드려요.',
 };
+
+export const dynamic = 'force-static';
+export const revalidate = false;
 
 function Announcement({ params }: { params: { id?: string } }) {
   return <AnnouncementContainer id={params.id} />;
