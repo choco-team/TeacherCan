eae2861616250a05f93e88a15894dfbea6705763
김홍동 <57981252+nlom0218@users.noreply.github.com>
2026-02-18 22:27:32 +0900

    feat: 개인정보처리방침 페이지 구현 (#116)

---
+++ src/app/legal/privacy-policy/page.tsx
@@ -0,0 +1,9 @@
+import PrivacyPolicyContainer from '@/containers/legal/privacy-policy-container';
+
+export const metadata = {
+  title: '개인정보처리방침',
+};
+
+export default function PrivacyPolicyPage() {
+  return <PrivacyPolicyContainer />;
+}
