5c42fb71fc9dbbc6f418005df7d99f202ea56bf0
김홍동 <57981252+nlom0218@users.noreply.github.com>
2026-04-17 14:46:50 +0900

    Revert "fix: 음악신청 서비스 에러 메시지 노출 (#125)"
    This reverts commit 8054f60d6c674d6a0b985497616307b91af012e5.

--- src/app/music-request/student/[roomId]/page.tsx
+++ src/app/music-request/student/[roomId]/page.tsx
@@ -1,11 +1,17 @@
-import MusicRequestMaintenanceNotice from '@/containers/music-request/music-request-maintenance-notice';
+import MusicRequestStudentContainer from '@/containers/music-request/music-request-student/music-request-student-container';
 
 export const metadata = {
   title: '음악신청',
 };
 
-function MusicRequestStudent() {
-  return <MusicRequestMaintenanceNotice />;
+function MusicRequestStudent({
+  params,
+}: {
+  params: {
+    roomId: string;
+  };
+}) {
+  return <MusicRequestStudentContainer params={params} />;
 }
 
 export default MusicRequestStudent;
