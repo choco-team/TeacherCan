2f5041492864024aeb182493e817b078acd903dc
soave424 <soave424@gmail.com>
2026-04-26 11:49:13 +0900

    직육면체 부피 구하기 추가

---
+++ src/app/skip-number/volumeblocks/page.tsx
@@ -0,0 +1,9 @@
+import VolumeBlocksContainer from '@/containers/skip-number/volume-blocks/volume-blocks-container';
+
+export const metadata = {
+  title: '직육면체 부피 구하기',
+};
+
+export default function VolumeBlocksPage() {
+  return <VolumeBlocksContainer />;
+}
